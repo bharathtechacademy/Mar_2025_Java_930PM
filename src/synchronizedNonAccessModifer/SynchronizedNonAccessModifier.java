7b4164ad11b4ac176783798b4a2ccefe6e58cec3
bharathautomation <soft.ittech20@gmail.com>
2025-04-29 23:12:04 +0530

    Non Access Mods

---
+++ src/synchronizedNonAccessModifer/SynchronizedNonAccessModifier.java
@@ -0,0 +1,13 @@
+package synchronizedNonAccessModifer;
+
+public class SynchronizedNonAccessModifier {
+	
+	//synchronized ==> one thread at a time
+	// can be used before the methods
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
